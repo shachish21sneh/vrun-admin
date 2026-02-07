a9db5389346a7e4429c75a169e5eeece4911bd46
Shachish <shachish@ajath.com>
2026-02-07 20:56:17 +0530

    Fix browser issue

--- src/components/sections/overview/recent-user-cars.tsx
+++ src/components/sections/overview/recent-user-cars.tsx
@@ -5,15 +5,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function RecentUserCars() {
   const { data, isLoading } = useGetUsersCarsQuery();
 
-const cars: UserCar[] = (data?.data ?? [])
-  .sort(
-    (a, b) =>
-      new Date(b.created_at).getTime() -
-      new Date(a.created_at).getTime(),
-  )
-  .slice(0, 10);
-  
-  
+const cars: UserCar[] = (data?.data ?? []).slice(0, 10);
+
   if (isLoading) {
     return (
       <div className="space-y-3">
