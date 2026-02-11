d15d0f8ca9cc8851fe65b5f1b59f20b1e87a8101
Shachish <shachish@ajath.com>
2026-02-11 14:01:06 +0530

    data table

--- src/components/sections/plans/plans-table/columns.tsx
+++ src/components/sections/plans/plans-table/columns.tsx
@@ -1,37 +1,21 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { Plan } from "@/types/plan";
-import { CellAction } from "./cell-action";
+import type { Plan } from "@/types";
 
 export const columns: ColumnDef<Plan>[] = [
   {
     accessorKey: "name",
-    header: "Plan Name"
+    header: "Name",
   },
   {
     accessorKey: "amount",
     header: "Amount",
-    cell: ({ row }) =>
-      `${row.original.currency} ${row.original.amount}`,
   },
   {
     accessorKey: "description",
     header: "Description",
   },
   {
-    accessorKey: "features",
-    header: "Features"
-  },
-  {
     accessorKey: "status",
     header: "Status",
-    cell: ({ row }) => (row.original.status ? "active" : "inactive")
   },
-  {
-    accessorKey: "createdAt",
-    header: "Created At"
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />
-  }
 ];
