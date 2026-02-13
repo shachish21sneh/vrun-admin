41825f6ceb3c1677be806f7d863cc5ec7c931145
Shachish <shachish@ajath.com>
2026-02-13 19:43:48 +0530

    Dashboard and list

--- src/components/sections/overview/view/overview.tsx
+++ src/components/sections/overview/view/overview.tsx
@@ -90,16 +90,16 @@ export default function OverViewPage() {
                     Total Revenue
                   </CardTitle>
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+					  xmlns="http://www.w3.org/2000/svg"
+					  viewBox="0 0 24 24"
+					  fill="none"
+					  stroke="currentColor"
+					  strokeLinecap="round"
+					  strokeLinejoin="round"
+					  strokeWidth="2"
+					  className="h-4 w-4 text-muted-foreground"
+					>
+                    <path d="M18 7H15.79a5.49 5.49 0 0 0-1-2H18a1 1 0 0 0 0-2H7a1 1 0 0 0 0 2h3.5a3.5 3.5 0 0 1 3.15 2H7a1 1 0 0 0 0 2h7a3.5 3.5 0 0 1-3.45 3H7a.7 .7 0 0 0-.14 0 .65 .65 0 0 0-.2 0 .69 .69 0 0 0-.19 .1l-.12 .07 0 0a.75 .75 0 0 0-.14 .17 1.1 1.1 0 0 0-.09 .14 .61 .61 0 0 0 0 .18A.65 .65 0 0 0 6 13s0 0 0 0a.7 .7 0 0 0 0 .14 .65 .65 0 0 0 0 .2 .69 .69 0 0 0 .1 .19s0 .08 .07 .12l6 7a1 1 0 0 0 1.52-1.3L9.18 14H10.5A5.5 5.5 0 0 0 16 9h2a1 1 0 0 0 0-2Z"/>
                   </svg>
                 </CardHeader>
                 <CardContent>
